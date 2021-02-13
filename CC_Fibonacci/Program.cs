621992a4c4ec9e49ce80a0a548b54faee4cf280a
tuda224 <johann.tuder@gmail.com>
2021-02-13 09:42:03 +0100

    initial commit

---
+++ CC_Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace CC_Fibonacci
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = ReadInput();
+            var splittedInput = input.Split(' ');
+            Console.WriteLine(input);
+        }
+
+        private static string ReadInput()
+        {
+            Console.WriteLine("Using input file: C:\\temp\\input.txt\nPress Return for run.");
+            Console.ReadLine();
+            var input = File.ReadAllText("C:\\temp\\input.txt");
+            return input;
+        }
+    }
+}
