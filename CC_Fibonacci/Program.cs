546e4f46b3184f4ef873327925214d096ca4f311
tuda224 <johann.tuder@gmail.com>
2021-02-13 10:35:47 +0100

    recursive and iterative solution

--- CC_Fibonacci/Program.cs
+++ CC_Fibonacci/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace CC_Fibonacci
@@ -7,9 +9,65 @@ namespace CC_Fibonacci
     {
         private static void Main(string[] args)
         {
-            var input = ReadInput();
-            var splittedInput = input.Split(' ');
-            Console.WriteLine(input);
+            while (true)
+            {
+                var input = ReadInput();
+                var positionOfNumber = int.Parse(input);
+                Stopwatch sw = new Stopwatch();
+
+                sw.Start();
+                var result = CalculateFibNumber(positionOfNumber);
+                sw.Stop();
+                var normalCalculation = sw.ElapsedTicks;
+
+                sw.Restart();
+                var recursiveResult = CalculateRecursive(positionOfNumber);
+                sw.Stop();
+                var recursiveCalculation = sw.ElapsedTicks;
+
+                Console.WriteLine($"result: {result} - {normalCalculation} ticks");
+                Console.WriteLine($"result: {recursiveResult} - {recursiveCalculation} ticks");
+            }
+        }
+
+        private static int CalculateRecursive(int positionOfNumber, int currentRun = 0, int last = 0, int preLast = 0)
+        {
+            if (currentRun == 0 || currentRun == 1)
+            {
+                last = currentRun;
+                return CalculateRecursive(positionOfNumber, ++currentRun, last, 0);
+            }
+            else if (currentRun < positionOfNumber)
+            {
+                var temp = last;
+                last += preLast;
+                preLast = temp;
+                return CalculateRecursive(positionOfNumber, ++currentRun, last, preLast);
+            }
+
+            return last + preLast;
+        }
+
+        private static int CalculateFibNumber(int positionOfNumber)
+        {
+            var last = 0;
+            var preLast = 0;
+            for (int i = 0; i <= positionOfNumber; i++)
+            {
+                // start with 0 and 1
+                if (i == 0 || i == 1)
+                {
+                    last = i;
+                }
+                // every following number sum of the two numbers before
+                else
+                {
+                    var temp = last;
+                    last = last + preLast;
+                    preLast = temp;
+                }
+            }
+            return last;
         }
 
         private static string ReadInput()
